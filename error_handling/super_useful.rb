d31c2568d48e9a483e341ea95e8132f1de786efc
Destiny Rose <destinyrose2358@gmail.com>
2019-08-07 12:25:07 -0500

    Finalizes the error_handling project

--- error_handling/super_useful.rb
+++ error_handling/super_useful.rb
@@ -1,6 +1,8 @@
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
+rescue ArgumentError
+  return nil
 end
 
 # PHASE 3
@@ -9,22 +11,52 @@ FRUITS = ["apple", "banana", "orange"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise Coffee_Error
   else 
-    raise StandardError 
+    raise StandardError
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue Coffee_Error
+    puts "Oh, I love coffee! I'll give you another chance."
+    retry
+  rescue StandardError
+    puts "What on earth is this!? Disgusting!"
+  end
+end
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+class Coffee_Error < StandardError
+  def message
+    print "coffee"
+  end
+end
 
+class Best_Friend_Poser_Error < StandardError
+  def message
+    puts "Wow, what a poser. You couldn't possibly be best friends."
+  end
+end
+
+class Empty_String_Error < StandardError
+  def message
+    puts "Empty string!"
+  end
+
+end
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    
+    raise Best_Friend_Poser_Error unless yrs_known >= 5
+    raise Empty_String_Error if name.length <= 0 || fav_pastime.length <= 0
+
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
